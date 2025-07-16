cb047a33c0ef2cd04577c489cac01c12339aa629
Prachi <prachi@gmail.com>
2025-07-17 02:05:32 +0530

    Arthmatic & Comparison Operator

---
+++ script1.js
@@ -0,0 +1,39 @@
+//Primitive data types are: Null,undefined, number, boolean, string, BigInt ,Symbol
+
+/*let a="Prachi";
+let b=null; //null typeof gives object 
+let c=undefined;
+let d=40;
+let e=2.5;
+const s=false; 
+
+console.log(typeof a,typeof b,typeof c,typeof d, typeof e,typeof s);*/
+
+//Arthmatic operator
+let a=2;
+let b=4;
+let c=a+b;
+let d=a*b;
+let e=a-b;
+let f=a/b;
+let g=a%b;
+let h=a**b; //  **power  */
+
+console.log(c);
+console.log(d);
+console.log(e);
+console.log(f);
+console.log(g);
+console.log(h);
+
+//Comparison Operator
+
+let p=40;
+let s="Hi"; //"40" // IN JavaScript "40" It accepts it is number 
+
+console.log(p==s);
+console.log(p===s); // strict comparison 
+
+console.log(p!=s);
+console.log(p!==s); //not equal value or not equal type
+
