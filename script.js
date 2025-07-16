e9d9f8c6fd0ef472b49b74804cc4162ff39b35fd
Prachi <prachi@gmail.com>
2025-07-16 11:35:11 +0530

    update file

--- script.js
+++ script.js
@@ -1,1 +1,39 @@
-console.log("This is the message from the external javascripe");
+//console.log("This is the message from the external javascripe");
+
+
+
+ //var a=10; // can be updated and redefined 
+
+// let a=30; //let me update kr saktai hai  not redefined 
+// a=50;
+
+//const a=15;
+
+//a=80;  //This will throw an error became 'a' is a constant
+
+//console.log(a);
+
+
+//var a; //Hosting  feature varaibale top pr pauch jayega that why it give undefine
+
+//console.log(a);
+
+//var a=30; //gives undefined 
+
+//let a=23; //it gives references 
+
+let a=20;
+let b=40;
+let c=a+b;
+let d=a*b;
+let e=a-b;
+let f=a/b;
+let g=a%b;
+
+console.log(c);
+console.log(d);
+console.log(e);
+console.log(f);
+console.log(g);
+
+
