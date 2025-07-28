04f3effe2944417d0d18ca50ff6e9123bdf8d1a4
Prachi <prachi@gmail.com>
2025-07-28 19:52:19 +0530

    Function 28-07-2025

---
+++ function.js
@@ -0,0 +1,50 @@
+// function
+
+function sum(a,b){
+    console.log(a+b);
+    console.log(a-b);
+
+}
+
+sum(10,20)
+sum(20,10)
+
+
+//function checkNumberOddAndEven(n){
+ //   if(n%2==0){
+  //      console.log("Number is even");
+ //   }
+
+  //  else{
+  //      console.log("Number is Odd");
+ //   }
+//}
+
+//checkNumberOddAndEven(20);
+//checkNumberOddAndEven(15);
+
+//Note:  0 is false and 1 is true in  Js 
+function checkNumberOddAndEven(n){
+     if(n%2){
+         console.log("Number is even");
+     }
+      else{
+        console.log("Number is Odd");
+      }
+
+    }
+
+    checkNumberOddAndEven(25); // o/p Number is even
+    checkNumberOddAndEven(30); //o/p Number is odd 
+
+    let j=10;
+    console.log("j=" +j);
+
+    let x=190;
+    let y=120;
+    let z=x+y;
+
+    console.log(x + "+" + y + "=" + z);
+
+   
+    
